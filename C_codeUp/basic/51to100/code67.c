36b5ede6c2d7cb33bc07afccca33ef4cd1854f76
rokisom <kyeongrok.mun@gmail.com>
2025-05-17 16:50:37 +0900

    change 67

---
+++ C_codeUp/basic/51to100/code67.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main (void)
+{
+    int a;
+    scanf("%d", &a);
+    if(a>0) { printf("plus\n");}
+    else { printf("minus\n");}
+
+    if(a%2==0) { printf("even\n");}
+    else { printf("odd\n");}
+    return 0;
+}
