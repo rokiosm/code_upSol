5cee9948798cdd52d49faaf0a7202f07ba0389e7
rokisom <kyeongrok.mun@gmail.com>
2025-05-18 18:27:32 +0900

    change 71

---
+++ C_codeUp/basic/51to100/code71.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int num1;
+    replay:
+        scanf("%d",&num1);
+        
+        if(num1 != 0){
+            printf("%d\n",num1);
+            goto replay;   
+        }
+        return 0;
+}
