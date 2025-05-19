af724097ef6edb7c3dcffa2a326e3a64be1b17e5
rokisom <kyeongrok.mun@gmail.com>
2025-05-19 21:06:05 +0900

    change 87

---
+++ C_codeUp/basic/51to100/code87.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int n, i=0, sum=0;
+    scanf("%d", &n);
+    do{
+        i++;
+        if(sum > n) { break;}
+        sum += i;
+    }
+    while(1);
+    printf("%d", sum);
+    return 0;
+}
