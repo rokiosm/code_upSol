46a2e930debc13beedf754b387a9795ecead9161
rokisom <kyeongrok.mun@gmail.com>
2025-05-21 17:18:00 +0900

    change 94

---
+++ C_codeUp/basic/51to100/code94.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, array[10000];
+    scanf("%d",&n);
+ 
+    for(int i=0; i<n; i++){
+        scanf("%d", &array[i]);
+    }
+    for(int i=n-1; i>=0; i--){
+        printf("%d ", array[i]);
+    }
+    return 0;
+}
